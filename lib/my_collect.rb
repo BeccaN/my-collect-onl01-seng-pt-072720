88f8497068bf901162fdf0262ce90e67f4dabc52
Rebecca Congi <rncongi@gmail.com>
2020-08-04 16:35:31 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,14 @@
 array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 def my_collect(array) do |name|
-  
-  
+  i = 0 
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1 
+  end 
 end 
 
+my_collect()
 
 
 # def hello(array)
