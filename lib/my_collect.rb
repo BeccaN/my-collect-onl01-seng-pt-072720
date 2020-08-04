d774a0337fe7dde4eb9e1d412193ef9cbd47fcf9
Rebecca Congi <rncongi@gmail.com>
2020-08-04 16:29:39 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,17 +3,13 @@
 
 
 
+# def hello(array)
+#   i = 0
+#   collection 
+#   while i < array.length
+#     collection << yield(array[i])
+#     i += 1
+#   end
+# end
 
-
-
-
-def hello(array)
-  i = 0
-  collection 
-  while i < array.length
-    collection << yield(array[i])
-    i += 1
-  end
-end
-
-hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
+# hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
