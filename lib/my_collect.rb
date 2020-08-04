d5d7cc0361068461c7c2a0cdb1b1246a03cb7e16
Rebecca Congi <rncongi@gmail.com>
2020-08-04 16:39:27 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -6,6 +6,7 @@ def my_collect(array)
     collection << yield(array[i])
     i += 1 
   end 
+  collection
 end 
 
 my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) {|name| name.split(" ").first}
